b38970b3546131a3356d688bc92e2386418d556c
Napatsakorn <ploy.napatsakorn@gmail.com>
2019-08-14 16:46:14 +0700

    update test

--- src/App.js
+++ src/App.js
@@ -5,25 +5,33 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <Post>
-        
-      </Post>
-      <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <center> 
+        <h1> you
+          </h1> 
+          <img src={logo} className="App-logo" alt="logo" />
+      </center>
+    
     </div>
+    // <div className="App">
+    //   <Post>
+        
+    //   </Post>
+    //   <header className="App-header">
+    //     {/* <img src={logo} className="App-logo" alt="logo" /> */}
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
   );
 }
 
